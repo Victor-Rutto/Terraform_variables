e6093b474f9cb75f0f2759ffc8794554fcec716f
kali <victorruttto1@gmail.com>
2024-03-14 17:48:15 +0300

    Number variables - EC2 Instances

---
+++ numbers.tf
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#VARIABLES - NUMBER - EC2 INSTANCE NUMBER
+
+provider "aws" {
+   region     = "us-east-1"
+   access_key = ""
+   secret_key = ""
+}
+
+resource "aws_instance" "ec2_example" {
+
+   ami           = "ami-02d7fd1c2af6eead0"
+
+   instance_type =  "t2.micro"
+   count = var.instance_count
+
+   tags = {
+           Name = "Terraform EC2"
+   }
+}
+
+variable "instance_count" {
+  description = "EC2 instance count"
+  type        = number
+  default     = 2
+}

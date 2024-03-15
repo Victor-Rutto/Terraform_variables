8e90bcc6a4f7829021ef1e2f136086295d24518e
kali <victorruttto1@gmail.com>
2024-03-15 11:20:20 +0300

    Using variable.tf file

---
+++ variables-tf/main.tf
@@ -0,0 +1,20 @@
+# Creating a variable.tf file that will be used besides this main.tf file
+
+
+provider "aws" {
+        region     = "us-east-1"
+        access_key = ""
+        secret_key = ""
+
+}
+
+ resource "aws_instance" "ec2_example" {
+
+   ami           = "ami-02d7fd1c2af6eead0"
+   instance_type =  var.instance_type
+	tags = {
+		Name= "Terraform EC2"
+}
+
+}
+
